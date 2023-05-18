b9fbeb889029da2bc9e89d64a382233d3824b82e
simonescoca <simone.scoca@icloud.com>
2023-05-18 14:47:55 +0200

    jsnack 1 completed

---
+++ jsnack1/script.js
@@ -0,0 +1,19 @@
+const numbersList = []
+sum = 0
+
+for (let i = 0 ; i < 10 ; i++) {
+    const number = parseFloat ( prompt ("Inserisci il " + (i + 1) + "º numero") )
+    if ( isNaN (number) ) {
+        console.log ("Perfavore, inserisci un numero")
+        i--
+    } else {
+        numbersList.push(number)
+        sum += number
+    }
+}
+
+for (let i = 0 ; i < 10 ; i++){
+    console.log ((i + 1) + ". " + numbersList[i])
+}
+
+console.log("Questi sono i numeri che hai scelto, e la loro somma è: " + sum)
